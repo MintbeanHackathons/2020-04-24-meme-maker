75f93d4eb27ddd5b7cc984530ffc603c9eda54cb
Sam Bernal <sambernaldamasco@gmail.com>
2020-04-24 15:02:24 -0400

    able to style a little bit

--- can-you-meme-this/src/App.js
+++ can-you-meme-this/src/App.js
@@ -5,19 +5,37 @@ import ImageEditor from './components/ImageEditor';
 
 function App() {
 	const [imgURL, setImgURL] = useState(null);
+	const [uploaded, setUploaded] = useState(false);
 
 	const handleUpload = (event) => {
 		setImgURL(URL.createObjectURL(event.target.files[0]));
 		console.log(event.target.files[0]);
+		setUploaded(true);
 	};
 
 	return (
 		<div className="App">
-			<div>
-				<input type="file" onChange={(event) => handleUpload(event)} />
-			</div>
+			<div className="container has-text-centered">
+				<h1 className="title is-1 is-spaced">CAN YOU MEME IT?</h1>
 
-			<ImageEditor imgURL={imgURL} />
+				<div class="file">
+					<label class="file-label">
+						<input
+							type="file"
+							className="file-input"
+							onChange={(event) => handleUpload(event)}
+						/>
+						<span class="file-cta">
+							<span class="file-icon">
+								<i class="fas fa-upload"></i>
+							</span>
+							<span class="file-label">Choose a file…</span>
+						</span>
+					</label>
+
+					{uploaded ? <ImageEditor imgURL={imgURL} /> : null}
+				</div>
+			</div>
 		</div>
 	);
 }
